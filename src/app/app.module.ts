d14e825a4391bdd7143cfacbac876a2d3ce11ba6
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-28 16:33:31 +0100

    feat: add AccommodationModule to import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from './layout/layout.module';
+import {AccommodationModule} from "./accommodation/accommodation.module";
 
 @NgModule({
   declarations: [
@@ -14,9 +15,10 @@ import { LayoutModule } from './layout/layout.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule
+    LayoutModule,
+	  AccommodationModule
   ],
-  
+
   providers: [],
   bootstrap: [AppComponent]
 })
