81679f0c1d00f8f8863ea44ee3028ad3703e2c71
Nikola <n.maric1912@gmail.com>
2023-12-06 20:02:09 +0100

    Add Service in User module with method to getUser from backend

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any> {
+    return this.http.get<any>('http://localhost:8080/api/users/2');
+  }
+}
