a2df722778d9825f990f1b0573f135e71935a6ee
Nikola <n.maric1912@gmail.com>
2023-12-09 18:07:15 +0100

    Add lodaing of comments and ratings about accommodation

--- src/app/user/update-profile/update-profile.component.spec.ts
+++ src/app/user/update-profile/update-profile.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UpdateProfileComponent } from './UpdateProfileComponent';
+import { UpdateProfileComponent } from './update-profile.component';
 
 describe('UpdateProfileComponent', () => {
   let component: UpdateProfileComponent;
