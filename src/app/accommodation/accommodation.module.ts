d43f821980b4c6002c90c421b35fe955ed3b6a87
pejinovics <pejinovicstefan@gmail.com>
2023-12-10 21:11:57 +0100

    feat: add picture upload and date pickers

--- src/app/accommodation/accommodation.module.ts
+++ src/app/accommodation/accommodation.module.ts
@@ -6,20 +6,24 @@ import {GoogleMapsModule} from "@angular/google-maps"
 import { RouterModule } from '@angular/router';
 import { AccommodationDetailsImagesComponent } from './accommodation-details-images/accommodation-details-images.component';
 import { AccommodationCreateComponent } from './accommodation-create/accommodation-create.component';
+import { AccommodationPictureUploadComponent } from './accommodation-picture-upload/accommodation-picture-upload.component';
+import {NgxDropzoneModule} from "ngx-dropzone";
 
 
 @NgModule({
   declarations: [
     AccommodationDetailsComponent,
 	AccommodationDetailsImagesComponent,
- AccommodationCreateComponent
+ AccommodationCreateComponent,
+ AccommodationPictureUploadComponent
   ],
 	imports: [
 		CommonModule,
 		NgOptimizedImage,
 		GoogleMapsModule,
 		RouterModule,
-		MaterialModule
+		MaterialModule,
+		NgxDropzoneModule
 	],
 	exports: [
 		AccommodationDetailsComponent,
