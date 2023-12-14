5e435a6fa09c4b19d36369af903287f064df34db
pejinovics <pejinovicstefan@gmail.com>
2023-12-14 18:11:17 +0100

    fix: add delete rows functionality

--- src/app/accommodation/pricelist/pricelist.component.ts
+++ src/app/accommodation/pricelist/pricelist.component.ts
@@ -16,6 +16,7 @@ export class PricelistComponent {
 
 	form: FormGroup;
 	data: DataRow[] = [];
+	currentId: number = 1;
 
 	constructor(private fb: FormBuilder) {
 		this.form = this.fb.group({
@@ -28,14 +29,60 @@ export class PricelistComponent {
 	addRow() {
 		if (this.form.valid) {
 			const newRow: DataRow = {
-				rowNumber: this.data.length + 1,
+				rowNumber: this.currentId++,
 				startDate: this.form.value.startDate,
 				endDate: this.form.value.endDate,
 				price: this.form.value.price
 			};
 
+			// Dodajte novi red
 			this.data.push(newRow);
+
+			// Resetujte formu
 			this.form.reset();
 		}
 	}
+
+
+
+	selectedRowIndexes: number[] = [];
+
+
+	selectRow(index: number): void {
+
+		const selectedIndex = this.selectedRowIndexes.indexOf(index);
+
+		if (selectedIndex === -1) {
+
+			this.selectedRowIndexes.push(index);
+		} else {
+
+			this.selectedRowIndexes.splice(selectedIndex, 1);
+		}
+	}
+
+
+	isRowSelected(index: number): boolean {
+		return this.selectedRowIndexes.indexOf(index) !== -1;
+	}
+
+
+	deleteSelectedRows(): void {
+		if (this.selectedRowIndexes.length > 0) {
+			const sortedIndexes = this.selectedRowIndexes.sort((a, b) => b - a);
+
+			sortedIndexes.forEach((index) => {
+				this.data.splice(index, 1);
+			});
+
+			this.resetCurrentId();
+			this.selectedRowIndexes = [];
+		}
+	}
+
+	resetCurrentId(): void {
+		const maxId = Math.max(...this.data.map(item => item.rowNumber), 0);
+		this.currentId = maxId + 1;
+	}
+
 }
