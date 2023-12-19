952a98bb761c317b13ce01d795fb361c3a5b5aa5
pejinovics <pejinovicstefan@gmail.com>
2023-12-19 11:49:41 +0100

    feat: add post for creating accommodation

--- src/app/accommodation/model/accommodation.model.ts
+++ src/app/accommodation/model/accommodation.model.ts
@@ -1,4 +1,8 @@
 import { Address } from "src/app/models/shared.models";
+import {ReservationMethod} from "./reservation-method.model";
+import {CancellationPolicy} from "./cancellation-policy.model";
+import {ApprovalStatus} from "./approval-status.model";
+import {PriceList} from "./price-list.model";
 
 export interface AccommodationDetails {
     id: number,
@@ -39,4 +43,40 @@ export interface AccommodationPopular{
     pricePerNight?: number;
     totalPrice?:number;
     image?: string;
+}
+
+export interface Accommodation{
+	id?: number | null;
+	title: string;
+	type: AccommodationType;
+	description: string;
+	cancellationPolicy: CancellationPolicy;
+	reservationMethod: ReservationMethod;
+	minGuest: number;
+	maxGuest: number;
+	address: Address;
+	amenities:Amenities[];
+	isPriceForEntireAcc: boolean;
+	accommodationApprovalStatus: ApprovalStatus;
+	hostId: number;
+	prices?: PriceList[] | null;
+	images?: string | null
+}
+
+export interface AccommodationCreate{
+	id: number;
+	title: string;
+	description: string;
+	amenities?: Amenities[];
+	images: string;
+	minGuests: number;
+	maxGuests: number;
+	type: AccommodationType;
+	cancellationPolicy: CancellationPolicy;
+	accommodationApprovalStatus: ApprovalStatus;
+	reservationMethod: ReservationMethod;
+	isPriceForEntireAcc: boolean;
+	prices: PriceList[];
+	hostId: number;
+	address: Address;
 }
