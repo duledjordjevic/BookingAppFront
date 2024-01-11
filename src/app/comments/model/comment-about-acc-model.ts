257d01ae72e46a212928443c85a62a9af29feb28
pejinovics <pejinovicstefan@gmail.com>
2024-01-11 18:50:58 +0100

    feat: add comment-about-acc model

---
+++ src/app/comments/model/comment-about-acc-model.ts
@@ -0,0 +1,15 @@
+import {Accommodation} from "../../accommodation/model/accommodation.model";
+
+export interface CommentAboutAcc{
+	id?: number;
+	rating?: number;
+	content?: string;
+	guestName?: string;
+	guestLastName?: string;
+	guestEmail?: string;
+	date?: Date;
+	accommodationId?: number;
+	guestId?: number;
+	accommodation?: Accommodation;
+	coverImage?: string;
+}
