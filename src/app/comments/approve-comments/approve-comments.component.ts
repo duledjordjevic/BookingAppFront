f9fa2d89274448a6919cb5570095daf5aadd31d6
pejinovics <pejinovicstefan@gmail.com>
2024-01-13 14:04:34 +0100

    feat: add approve-comments component

---
+++ src/app/comments/approve-comments/approve-comments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-approve-comments',
+  templateUrl: './approve-comments.component.html',
+  styleUrls: ['./approve-comments.component.css']
+})
+export class ApproveCommentsComponent {
+
+}
--- src/app/comments/comments.module.ts
+++ src/app/comments/comments.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { CommentHostComponent } from './comment-host/comment-host.component';
 import {MaterialModule} from "../infrastructure/material/material.module";
 import { CommentAccComponent } from './comment-acc/comment-acc.component';
+import { ApproveCommentsComponent } from './approve-comments/approve-comments.component';
 
 @NgModule({
   declarations: [
     CommentHostComponent,
-    CommentAccComponent
+    CommentAccComponent,
+    ApproveCommentsComponent
   ],
 	imports: [
 		CommonModule,
