015be28657e281ea1d46144573ae93d9ce9dc713
pejinovics <pejinovicstefan@gmail.com>
2024-01-14 13:35:20 +0100

    feat: add report comment popup

---
+++ src/app/comments/report-comment-popup/report-comment-popup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-comment-popup',
+  templateUrl: './report-comment-popup.component.html',
+  styleUrls: ['./report-comment-popup.component.css']
+})
+export class ReportCommentPopupComponent {
+
+}
