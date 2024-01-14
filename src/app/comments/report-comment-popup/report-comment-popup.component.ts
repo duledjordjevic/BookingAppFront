976638e70f04ab17f8a7bcc828f0bbf6ebb56554
pejinovics <pejinovicstefan@gmail.com>
2024-01-14 15:32:59 +0100

    feat: add functionality to report comment popup

--- src/app/comments/report-comment-popup/report-comment-popup.component.ts
+++ src/app/comments/report-comment-popup/report-comment-popup.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {ReportUserService} from "../../accommodation/services/report-user.service";
+import {UserReport} from "../../accommodation/model/user-report";
+import {CommentsService} from "../services/comments.service";
 
 @Component({
   selector: 'app-report-comment-popup',
@@ -6,5 +10,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./report-comment-popup.component.css']
 })
 export class ReportCommentPopupComponent {
+	commentAccId: number | undefined = 0;
+	commentHostId: number | undefined = 0;
+	isAcc: boolean = true;
+	firstname: string = "";
+	lastName: string = "";
+	title: string = "";
 
+	constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<ReportCommentPopupComponent>,
+				private commentsService: CommentsService) {
+		this.firstname = data.name;
+		this.lastName = data.lastName;
+		this.commentAccId = data.commentAccId;
+		this.commentHostId = data.commentHostId;
+		this.isAcc = data.isAcc;
+
+	}
+	reportReason: string = "";
+	validator: boolean = false;
+	idToSend: number | undefined = 0;
+
+	reportCommentAboutAcc(commentId?: number ): void {
+		this.commentsService.reportCommentAboutAcc(commentId as number, true).subscribe({
+			next: () => {
+				// this.getCommentsAboutAcc();
+				console.log("Uspesno reportovanje komentara");
+			},
+			error: () => {
+				console.log("Error reporting comment");
+			}
+		})
+	}
+
+	reportCommentAboutHost(commentId?: number ): void {
+		this.commentsService.reportCommentAboutHost(commentId as number, true).subscribe({
+			next: () => {
+				// this.getCommentsAboutHost();
+				console.log("Uspesno reportovanje komentara");
+			},
+			error: () => {
+				console.log("Error reporting comment");
+			}
+		})
+	}
+
+	closeDialog(): void {
+		this.dialogRef.close();
+	}
+
+	reportUser(): void{
+		this.validator = false;
+		if(this.reportReason.length < 10){
+			this.validator = true;
+			return;
+		}
+		this.idToSend = this.isAcc ? this.commentAccId : this.commentHostId;
+
+		if (this.isAcc){
+			this.commentsService.setReportMessageAcc(this.commentAccId as number, this.reportReason).subscribe({
+				next:(_) => {
+					this.reportCommentAboutAcc(this.commentAccId);
+					console.log("Uspesno sam reportovao sa popupom")
+				}
+			})
+		}
+		else {
+			this.commentsService.setReportMessageHost(this.commentHostId as number, this.reportReason).subscribe({
+				next:(_) => {
+					this.reportCommentAboutHost(this.commentHostId);
+					console.log("Uspesno sam reportovao sa popupom")
+				}
+			})
+		}
+
+		this.dialogRef.close(true);
+	}
 }
